99de8d2c06d7f77be52cb0f6a4390d92e3a75343
Brandon <brandonbb99@gmail.com>
2024-01-29 20:59:17 +0800

    body fix

--- app.py
+++ app.py
@@ -14,7 +14,12 @@ def hello_world():  # put application's code here
 def buildheader_body():
     accesstoken = "ya29.a0AfB_byBaYuMqumy8XMdZ-Xl66pfTcj_uUj0fMm6u81PB5RCro6iN_rZZeyuUFiNI40BCcQYFTgumbTbkpPqLJ9oaKcjoj9l1snRREzwzQUiI0M771loIoIIoNrRI4UQ6BRJ46gSy9Jg1TQVmRJwsAKoKWEvpc6emI86YjBvns65CgbT5I-D01WFm0jZht4r5Udg6JK1zsSC8fl7Ct2vnwc7Txl_KG38VAiAQkCT51mJ1DpRKE9CfiUI7e52GH05atMgAgtAIqEmrXfxHo45AzX-vq-mTdFNQ4T8OqLWHCAnuLdWnGg0-lTZkYLyZnczt1db2N5psoggwiD6rC6t1vjNJY2DHqqlPwuf5jhfuF69ezW4KxYcGDBx5dqXxlaJ5H_HjvhUogVoCU1sonyjH7OwdW2YP6rkaCgYKAU4SARMSFQHGX2MikYA-WIcNdTDOMFK8-S0mlQ0422"
     url = "https://us-central1-aiplatform.googleapis.com/v1/projects/healthhack-412317/locations/us-central1/publishers/google/models/text-bison:predict?access_token="+accesstoken
-    payload ={
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    data = {
         "instances": [
             {
                 "context": "You are to explain medical reports to people who does not understand complex terms. Please breakdown the medical jargons and explain it to them as simply as you can. Explain if it is a positive or negative result",
@@ -26,15 +31,14 @@ def buildheader_body():
                         },
                         "output": {
                             "author": "bot",
-                            "content": "These are organisms that cause infections. Trichomonas is a protozoan parasite causing the sexually transmitted infection trichomoniasis, primarily affecting the urogenital tract in both men and women.\
-        While Candida is a genus of yeast, with Candida albicans being a common species that can cause various infections, including yeast infections in the genital or oral areas, skin, nails, and mucous membranes"
+                            "content": "These are organisms that cause infections. Trichomonas is a protozoan parasite causing the sexually transmitted infection trichomoniasis, primarily affecting the urogenital tract in both men and women. While Candida is a genus of yeast, with Candida albicans being a common species that can cause various infections, including yeast infections in the genital or oral areas, skin, nails, and mucous membranes"
                         }
                     }
                 ],
                 "messages": [
                     {
                         "author": "user",
-                        "content": "Trichomonas or Candida organisms are seen."
+                        "content": "Trichomonas or Candida organisms are seen"
                     }
                 ]
             }
@@ -46,11 +50,8 @@ def buildheader_body():
             "topP": 1
         }
     }
-    headers = {
-        'Content-Type': 'application/json'
-    }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, json=data)
 
     print(response.text)
     return response.text
